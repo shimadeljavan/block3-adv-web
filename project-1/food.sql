ebf6cea1ed27911eb5d0b6cb8b3cf0a2ebb85e8d
shimadeljavan <134743007+shimadeljavan@users.noreply.github.com>
2023-12-04 23:05:03 -0500

    EDIT

--- project-1/food.sql
+++ project-1/food.sql
@@ -1,26 +1,34 @@
-CREATE TABLE `shima94_food`.`suppliers` (`supplierID` INT NOT NULL AUTO_INCREMENT , `supplierName` VARCHAR(255) NOT NULL , `address` VARCHAR(255) NOT NULL , `contactNumber` VARCHAR(255) NOT NULL , PRIMARY KEY (`supplierID`)) ENGINE = InnoDB;
-INSERT INTO `suppliers` (`supplierID`, `supplierName`, `address`, `contactNumber`) VALUES (NULL, 'Supplier 1', '123 Main Street', '555-1234');
+CREATE TABLE `shima94_food`.`suppliers` (`supplierID` INT NOT NULL AUTO_INCREMENT , `supplierName` VARCHAR NOT NULL , `address` VARCHAR NOT NULL , `contactNumber` VARCHAR NOT NULL , PRIMARY KEY (`supplierID`)) ENGINE = InnoDB;
 
+CREATE TABLE `shima94_food`.`Ingredients` (`ingredientID` INT NOT NULL AUTO_INCREMENT  , `ingredientName` VARCHAR(255) NOT NULL , `type` VARCHAR(255) NOT NULL , `price` VARCHAR(255) NOT NULL , PRIMARY KEY (`ingredientID`)) ENGINE = InnoDB;
 
-CREATE TABLE `shima94_food`.`ingredient` (`ingredientID` INT NOT NULL AUTO_INCREMENT , `ingredientName` VARCHAR(255) NOT NULL , `type` VARCHAR(255) NOT NULL , `price` VARCHAR(255) NOT NULL , `supplierID` INT NOT NULL, PRIMARY KEY (`ingredientID`)) ENGINE = InnoDB;
-INSERT INTO `ingredient` (`ingredientID`, `ingredientName`, `type`, `price`, `supplierID`) VALUES (NULL, 'Flour', 'Grain', '1.99', '1');
+CREATE TABLE `shima94_food`.`dishes` (`disheID` INT NOT NULL AUTO_INCREMENT , `dishName` VARCHAR(255) NOT NULL , `Price` VARCHAR(255) NOT NULL , PRIMARY KEY (`disheID`)) ENGINE = InnoDB;
 
-CREATE TABLE `shima94_food`.`Dishes` (`dishID` INT NOT NULL AUTO_INCREMENT , `dishName` VARCHAR(255) NOT NULL , `price` VARCHAR(255) NOT NULL , PRIMARY KEY (`dishID`)) ENGINE = InnoDB;
-INSERT INTO `Dishes` (`dishID`, `dishName`, `Price`) VALUES (NULL, 'Margherita Pizza', '9.99');
+CREATE TABLE `shima94_food`.`foodVendor` (`foodVendorID` INT NOT NULL AUTO_INCREMENT , `vendorName` VARCHAR(255) NOT NULL , `dishID` INT NOT NULL , `supplierID` INT NOT NULL , PRIMARY KEY (`foodVendorID`)) ENGINE = InnoDB;
 
-CREATE TABLE `shima94_food`.`FoodVendor` (`vendorID` INT NOT NULL AUTO_INCREMENT , `vendorName` VARCHAR(255) NOT NULL , `dishID` INT NOT NULL , `supplierID` INT NOT NULL , PRIMARY KEY (`vendorID`)) ENGINE = InnoDB;
+ALTER TABLE `foodVendor` ADD CONSTRAINT `dishID` FOREIGN KEY (`dishID`) REFERENCES `dishes`(`disheID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-ALTER TABLE `ingredient` ADD CONSTRAINT `suppelierID` FOREIGN KEY (`supplierID`) REFERENCES `suppliers`(`supplierID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `foodVendor` DROP FOREIGN KEY `dishID`; ALTER TABLE `foodVendor` ADD CONSTRAINT `supplierID` FOREIGN KEY (`dishID`) REFERENCES `dishes`(`disheID`) ON DELETE RESTRICT ON UPDATE RESTRICT; ALTER TABLE `foodVendor` ADD FOREIGN KEY (`supplierID`) REFERENCES `suppliers`(`supplierID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-ALTER TABLE `FoodVendor` ADD CONSTRAINT `dishId` FOREIGN KEY (`dishID`) REFERENCES `Dishes`(`dishID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `ingredients` ADD CONSTRAINT `supplierID` FOREIGN KEY (`supplierID`) REFERENCES `suppliers`(`supplierID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-ALTER TABLE `FoodVendor` ADD CONSTRAINT `supplierID` FOREIGN KEY (`supplierID`) REFERENCES `suppliers`(`supplierID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE `foodVendor` ADD CONSTRAINT `ingredientID` FOREIGN KEY (`ingredientID`) REFERENCES `Ingredients`(`ingredientID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
-ALTER TABLE `FoodVendor` ADD CONSTRAINT `ingredientID` FOREIGN KEY (`ingredientID`) REFERENCES `ingredient`(`ingredientID`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+INSERT INTO `dishes` (`disheID`, `dishName`, `Price`) VALUES (NULL, 'Pizza', '10.00'), (NULL, 'Salad', '8.50');
 
-INSERT INTO `FoodVendor` (`vendorID`, `vendorName`, `dishID`, `supplierID`, `ingredientID`) VALUES (NULL, 'Vendor1', '4', '4', '4'), (NULL, 'Vendor2', '5', '5', '6');
+INSERT INTO `Ingredients` (`ingredientID`, `ingredientName`, `type`, `price`) VALUES (NULL, 'Flour', 'Grain', '1.99'), (NULL, 'Cheese', 'Dairy', '3.99');
 
-SELECT * FROM `FoodVendor` NATURAL JOIN Dishes;
+INSERT INTO `suppliers` (`supplierID`, `supplierName`, `address`, `contactNumber`) VALUES (NULL, 'Supplier 1', '123 Main Street', '555-1234'), (NULL, 'Supplier 2', '456 Oak Avenue', '555-5678');
 
-SELECT * FROM `FoodVendor` NATURAL JOIN Dishes
+INSERT INTO `foodVendor` (`foodVendorID`, `vendorName`, `dishID`, `supplierID`, `ingredientID`) VALUES (NULL, 'Vendor1', '1', '1', '2'), (NULL, 'Vendor2', '2', '2', '1');
+
+SELECT * FROM foodVendor NATURAL JOIN dishes NATURAL JOIN suppliers NATURAL JOIN ingredients; 
+
+SELECT * FROM foodVendor NATURAL JOIN Ingredients;
+
+SELECT * FROM foodVendor NATURAL JOIN dishes;
+
+SELECT * FROM foodVendor NATURAL JOIN dishes ORDER BY vendorName;
+
+SELECT * FROM foodVendor NATURAL JOIN dishes ORDER BY vendorName DESC, dishName ASC;
 
